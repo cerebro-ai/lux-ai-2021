21dd4e014ae82082c6ae16c536d8ad0e88b5d46f
Erik Steiger <rkstgr@gmail.com>
2021-10-06 22:03:18 +0200

    add CustomActorCriticPolicy

---
+++ src/models/policy.py
@@ -0,0 +1,41 @@
+from collections import Callable
+from typing import Optional, List, Union, Dict, Type
+
+import gym
+from stable_baselines3.common.policies import ActorCriticPolicy
+from torch import nn
+
+from src.models.action_net import CustomNetwork, CustomMlpExtractor
+
+
+class CustomActorCriticPolicy(ActorCriticPolicy):
+    def __init__(
+            self,
+            observation_space: gym.spaces.Space,
+            action_space: gym.spaces.Space,
+            lr_schedule: Callable[[float], float],
+            net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = None,
+            activation_fn: Type[nn.Module] = nn.Tanh,
+            *args,
+            **kwargs,
+    ):
+        super(CustomActorCriticPolicy, self).__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            # Pass remaining arguments to base class
+            *args,
+            **kwargs,
+        )
+        # Disable orthogonal initialization
+        self.ortho_init = False
+
+    def _build_mlp_extractor(self) -> None:
+        self.mlp_extractor = CustomMlpExtractor(
+            self.features_dim,
+            net_arch=self.net_arch,
+            activation_fn=self.activation_fn,
+            device=self.device,
+        )
