e3a4c4ebcf4ff57311343d0772df1136563ca9b0
Erik Steiger <rkstgr@gmail.com>
2021-10-07 09:34:41 +0200

    constants.py

---
+++ src/models/constants.py
@@ -0,0 +1,15 @@
+# states
+# map
+map_channels = 18
+map_height = 32
+
+# state
+state_size = 5
+
+# unit
+unit_size = 4
+
+# actions
+unit_action_space = 10
+city_action_space = 3
+total_action_size = max(unit_action_space, city_action_space)
