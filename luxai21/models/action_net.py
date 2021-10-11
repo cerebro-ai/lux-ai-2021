9484644ef4271225d1d37cac4ea787ee96f351b0
fxbk <44337498+fxbk@users.noreply.github.com>
2021-10-11 16:21:07 +0200

    Fix: Updated return in forward to masked_action

--- luxai21/models/action_net.py
+++ luxai21/models/action_net.py
@@ -73,7 +73,7 @@ class CustomMlpExtractor(MlpExtractor):
         action_mask = th.narrow(action_mask, 1, 0, action_logits.shape[1])
         masked_action_logits = action_logits.masked_fill(~action_mask.to(th.bool), float("-inf"))
 
-        return action_logits, value
+        return masked_action_logits, value
 
 
 class CustomNetwork(nn.Module):
