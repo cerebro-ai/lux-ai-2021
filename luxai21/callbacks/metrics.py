09bf4c6eb2bc7a11609636f7e8b01321551c2408
Erik Steiger <rkstgr@gmail.com>
2021-11-07 17:27:06 +0100

    Feat: Implement custom logger
    Logs:
    - win_rate
    - number of city_tiles at the end of the best game
    - number of city_tiles at the end of the worst game
    - replay of best game
    - replay of worst game

--- luxai21/callbacks/metrics.py
+++ luxai21/callbacks/metrics.py
@@ -1,3 +1,6 @@
+import tempfile
+from copy import copy
+from pathlib import Path
 from typing import Dict, Optional
 
 import numpy as np
@@ -11,6 +14,7 @@ from ray.rllib.policy import Policy
 from ray.rllib.policy.sample_batch import SampleBatch
 from ray.rllib.utils.typing import PolicyID
 from ray.tune import Callback
+from wandb import util
 
 from luxai21.env.lux_ma_env import LuxMAEnv
 
@@ -72,8 +76,10 @@ class MetricsCallbacks(DefaultCallbacks):
         episode.custom_metrics["game_won"] = 1 if env.game_state.get_winning_team() == 0 else 0
 
         try:
-            episode.media["replay"] = wandb.Html(env.render("html"), inject=False)
-            print("save replay")
+            _, filepath = tempfile.mkstemp("_lux-replay_" + util.generate_id() + ".html")
+            with Path(filepath).open("w") as f:
+                f.write(env.render("html"))
+            episode.media["replay"] = wandb.Html(filepath, inject=False)
         except AttributeError as e:
             print(f"Could not generate replay: {e}")
             print(f"Game match_over should be True:", {env.game_state.match_over()})
@@ -85,27 +91,40 @@ class MetricsCallbacks(DefaultCallbacks):
 
     def on_sample_end(self, *, worker: "RolloutWorker", samples: SampleBatch,
                       **kwargs) -> None:
-        print("returned sample batch of size {}".format(samples.count))
+        pass
 
     def on_train_result(self, *, trainer, result: dict, **kwargs):
 
         if "replay" in result["episode_media"].keys():
+            n_replays = result["episodes_this_iter"]
+            print(f"Number collected replays: {n_replays}")
+
             # compute the score of each game, given end city_tiles and worker
-            game_score = 1000 * np.array(result["hist_stats"]["end_player_city_tiles"]) + np.array(
-                result["hist_stats"]["end_player_worker"])
+            game_score = 1000 * np.array(result["hist_stats"]["end_player_city_tiles"][-n_replays:]) + np.array(
+                result["hist_stats"]["end_player_worker"][-n_replays:])
 
             # Pick the best game
             best_game_index = np.argmax(game_score)
             print("log best game with score", game_score[best_game_index])
-            result["episode_media"]["best_game"] = result["episode_media"]["replay"][best_game_index]
+            print("best game replay", result["episode_media"]["replay"][-n_replays:][best_game_index]._path)
+            result["episode_media"]["best_game"] = copy(result["episode_media"]["replay"][-n_replays:][best_game_index])
+
+            # store the number of city_tiles of the best game
+            result["custom_metrics"]["best_game_city_tiles"] = \
+                result["hist_stats"]["end_player_city_tiles"][-n_replays:][best_game_index]
 
             # Pick the worst game
             worst_game_index = np.argmin(game_score)
             print("log worst game with score", game_score[worst_game_index])
-            result["episode_media"]["worst_game"] = result["episode_media"]["replay"][worst_game_index]
+            result["episode_media"]["worst_game"] = copy(
+                result["episode_media"]["replay"][-n_replays:][worst_game_index])
+
+            # store the number of city_tiles of the worst game
+            result["custom_metrics"]["worst_game_city_tiles"] = \
+                result["hist_stats"]["end_player_city_tiles"][-n_replays:][worst_game_index]
 
             # Delete all other replays
-            del result["episode_media"]["replay"]
+            result["episode_media"]["replay"] = []
 
         # Rename metric game_won_mean to win_rate and remove other metrics
         if "game_won_mean" in result["custom_metrics"]:
